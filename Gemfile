0c37b923d10addf513dc3952fc9fdf107ca44a1d
Fatema Akbar <fakbar@qatar.cmu.edu>
2012-11-29 13:41:15 +0300

    config

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ gem 'rails', '3.2.1'
 #gem 'sqlite3'
 
 group :development do
-  gem 'sqlite3', '1.3.5'
+  gem 'sqlite3'
 end
 
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,7 @@ SeniorProject::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.assets.precompile += %w( defaults.js )
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
 
