1d78fe612732baca6cd0581dd77eff6697c65efa
Fatema <fakbar@qatar.cmu.edu>
2012-12-09 23:53:13 +0300

    css fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_url, :notice => "New user added. You are now logged in."
+      redirect_to root_url
     else
       render :action => 'new'
     end
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
   def update
     @user = current_user
     if @user.update_attributes(params[:user])
-      redirect_to root_url, :notice => "Your profile has been updated."
+      redirect_to root_url
     else
       render :action => 'edit'
     end
